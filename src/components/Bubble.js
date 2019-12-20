0f4f39a0f9889fc1ef954d1a35caa073a75582cd
Kristedd <kristiangerkman1@gmail.com>
2019-12-20 13:17:35 +0200

    added bubble sort

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -6,6 +6,34 @@ const Bubble = () => {
 
   const sort = event => {
     event.preventDefault();
+
+    const iterate = (x, y) => {
+      let tmp = x;
+      x = y;
+      y = tmp;
+
+      return { x, y };
+    };
+
+    const wait = ms => {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    };
+
+    const bubbleSort = async (arr, ms) => {
+      for (let i = 0; i < input - 1; i++) {
+        for (let j = 0; j < input - i - 1; j++) {
+          await wait(ms);
+          if (arr[j] > arr[j + 1]) {
+            let asd = iterate(arr[j], arr[j + 1]);
+            arr[j] = asd.x;
+            arr[j + 1] = asd.y;
+          }
+          //render cooool graphics
+          console.log(arr);
+        }
+      }
+    };
+
     if (isNaN(input)) {
       console.log("please make sure your input is a numeric value");
     } else if (input > 300 || input < 20) {
@@ -15,6 +43,7 @@ const Bubble = () => {
       arr = Array.from({ length: Number(input) }, () =>
         Math.floor(Math.random() * Number(input))
       );
+      bubbleSort(arr, 1000);
       console.log(arr);
     }
   };
